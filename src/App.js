c2fbe653d085f29a9cc9b79bb0ac987147c745da
patrik909 <patrik.eriksson@medieinstitutet.se>
2019-03-03 10:42:05 +0100

    SASS added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 class App extends Component {
+
+  state = {
+    posts: []
+  }
+
+  componentDidMount() {
+    
+    fetch('http://localhost:8888/wp-api/wp-json/wp/v2/posts')
+    .then((res) => res.json())
+    .then((posts) => {
+      this.setState({ posts });
+      console.log(posts);
+    });
+
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <main>
+          { 
+            this.state.posts.length !== 0 ? 
+             this.state.posts.map(post => {
+              return (
+                <div key={post.id}>
+                  <h1>{post.title.rendered}</h1>
+                  <div className="img-holder">
+                    <img src={post.featured_image_url} alt={post.featured_image_url} />
+                  </div>
+                  <h3>{post.excerpt.rendered}</h3>
+                  <p>{post.acf.a_field}</p>
+                </div>
+              )
+            })  
+            : 
+            <div>
+              waiting...
+            </div>
+          }
+        </main>
       </div>
     );
   }
